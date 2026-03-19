5f594190a29dfdf11bf8dbce8bfaf7190804d750
Michael Allen <michaelallen@pdx1-shared-a4-04.dreamhost.com>
2026-03-19 15:53:52 -0700

    fixed images over parallax for some use cases, various style fixes, various base template fixes

--- css/mainmenu.php
+++ css/mainmenu.php
@@ -4,10 +4,10 @@ $isMainMenuFluid = $params->get('fluidMainMenu');
 
 <?php if(!$this->params->get('mainmenuHasRoundedCorners')) : ?>
 	.navbar-mainmenu {
-		border-radius: 0;
+		border-radius: 0 !important;
 	}
 
 	.navbar-mainmenu .navbar-nav > li > a {
-		border-radius: 0;
+		border-radius: 0 !important;
 	}
 <?php endif; ?>
